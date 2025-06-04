a67889fb2ae2ca622eb49405349aabbe0618b042
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-06-04 17:42:55 +0530

    Update CategoryExplorer.tsx

--- src/components/CategoryExplorer.tsx
+++ src/components/CategoryExplorer.tsx
@@ -24,7 +24,7 @@ const CategoryExplorer: React.FC = () => {
     },
     {
       id: 3,
-      image: 'src/assets/shoe.JPG',
+      image: 'https://drive.google.com/file/d/1dTwoppnFkjcbZ5_rZwGn5znX2t9dUYGP/view?usp=drive_link',
       title: 'Travel Essentials',
       description: 'Luxury companions for your journey'
     },
